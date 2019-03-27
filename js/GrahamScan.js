eec0a3c5446fd9777fe0b02107c3ac850e7b4adc
PScottZero <8pscott@gmail.com>
2019-03-27 15:21:25 -0400

    27 March 2019
    updated Graham Scan algorithm

--- js/GrahamScan.js
+++ js/GrahamScan.js
@@ -1,9 +1,15 @@
 /**
  * @author Paul Scott
- * @version 26 March 2019
+ * @version 27 March 2019
  *
  * code that starts and resets convex hull algorithm
  */
+
+// constants
+const X = 0; // x value
+const Y = 1; // y value
+const ANG = 2; // angle
+const INC = 3; // in convex (boolean)
 
 class ConvexHull {
 
@@ -14,21 +20,9 @@ class ConvexHull {
     this.points = []; // set of points
     this.stack = []; // stack
     this.delay = 1000; // animation delay
-    this.finished = false;
-    this.width = 600;
-    this.currentPoint = null;
-  }
-
-  /**
-   * compares point angles for sorting purposes
-   * @param point1
-   * @param point2
-   * @returns {number}
-   */
-  static compare(point1, point2) {
-    if (point1.angle < point2.angle) return -1;
-    if (point1.angle > point2.angle) return 1;
-    else return 0;
+    this.width = 600; // canvas width
+    this.finished = false; // finished status
+    this.currentPoint = null; // current point being checked
   }
 
   /**
@@ -36,6 +30,8 @@ class ConvexHull {
    * @returns {Promise<void>}
    */
   async grahamScan() {
+
+    // hide reset button
     document.getElementById("reset").style.display = "none";
 
     // creates random set of points
@@ -48,63 +44,39 @@ class ConvexHull {
 
     // sorts points by angle
     this.points.sort(ConvexHull.compare);
-    this.draw();
-    await new Promise(resolve => setTimeout(resolve, this.delay));
+    await this.drawAndWait();
 
     // pushes first three point indexes to stack
-    this.stack.push(0);
-    this.stack.push(1);
-    this.draw();
-    await new Promise(resolve => setTimeout(resolve, this.delay));
-    this.stack.push(2);
-    this.draw();
-    await new Promise(resolve => setTimeout(resolve, this.delay));
+    this.stack.push(this.points[0]);
+    this.stack.push(this.points[1]);
+    await this.drawAndWait();
+    this.stack.push(this.points[2]);
+    await this.drawAndWait();
 
     // loops through remaining points
     for (let i = 3; i < this.points.length; i++) {
       while (this.stack.length !== 0) {
         let p1 = this.stack[this.stack.length - 2];
         let p2 = this.stack[this.stack.length - 1];
-        this.currentPoint = i;
-        this.draw();
-        await new Promise(resolve => setTimeout(resolve, this.delay));
+        this.currentPoint = this.points[i];
+        await this.drawAndWait();
 
         // calculates cross product of three points
         // pops stack if 'right turn'
         // pushes index to stack if 'left turn'
-        if (ConvexHull.crossProduct(this.points[p1], this.points[p2], this.points[i]) < 0) {
-          this.points[this.stack.pop()].inHull = false;
+        if (ConvexHull.crossProduct(p1, p2, this.currentPoint) < 0) {
+          this.stack.pop()[INC] = false;
         } else {
-          this.stack.push(i);
+          this.stack.push(this.points[i]);
           break;
         }
       }
     }
     this.finished = true;
     this.draw();
+
+    // make reset button visible
     document.getElementById("reset").style.display = "block";
-  }
-
-  /**
-   * calculates cross product of
-   * vectors P1P2--> and P1P3-->
-   * @param point1
-   * @param point2
-   * @param point3
-   * @returns {number}
-   */
-  static crossProduct(point1, point2, point3) {
-
-    // vector P1P2-->
-    let vector1X = point2.x - point1.x;
-    let vector1Y = point2.y - point1.y;
-
-    // vector P1P3-->
-    let vector2X = point3.x - point1.x;
-    let vector2Y = point3.y - point1.y;
-
-    // cross product
-    return (vector1X * vector2Y) - (vector1Y * vector2X);
   }
 
   /**
@@ -115,46 +87,90 @@ class ConvexHull {
     for (let i = 0; i < pointCount; i++) {
       let x = Math.floor((Math.random() * (this.width - 99)));
       let y = Math.floor((Math.random() * (this.width - 99)));
-      let point = new PointAngle(x, y, 0.0, true);
-      this.points.push(point);
+      this.points.push([x, y, 0.0, true]);
     }
   }
 
   /**
+   * calculates cross product of
+   * vectors P1->P2 and P1->P3
+   * @param p1 - point 1
+   * @param p2 - point 2
+   * @param p3 - point 3
+   * @returns {number} - cross product
+   */
+  static crossProduct(p1, p2, p3) {
+
+    // vector P1->P2 (A) and vector P1->P3 (B)
+    let vectorA = [p2[0] - p1[0], p2[1] - p1[1]];
+    let vectorB = [p3[0] - p1[0], p3[1] - p1[1]];
+
+    // cross product
+    return (vectorA[0] * vectorB[1]) - (vectorA[1] * vectorB[0]);
+  }
+
+  /**
+   * compare angles of points p1 and p2
+   * @param p1 - point 1
+   * @param p2 - point 2
+   * @returns {number}
+   */
+  static compare(p1, p2) {
+    if (p1[ANG] < p2[ANG]) return -1;
+    if (p1[ANG] > p2[ANG]) return 1;
+    else return 0;
+  }
+
+  /**
    * returns index of point with smallest y value
-   * @returns number of point with lowest y value
+   * @returns minimum point
    */
   getMinY() {
-    let minIndex = null;
+    let min = null;
     for (let i = 0; i < this.points.length; i++) {
-      if (minIndex == null || this.points[minIndex].y > this.points[i].y) {
-        minIndex = i;
+      if (min == null || min[Y] > this.points[i][Y]) {
+        min = this.points[i];
       }
     }
-    return minIndex;
+    return min.slice();
   }
 
   /**
    * calculates angles of all points based on
    * point with lowest y value
-   * @param minIndex
+   * @param min - minimum point
    */
-  getAngles(minIndex) {
-    let scaleX = this.points[minIndex].x;
-    let scaleY = this.points[minIndex].y;
+  getAngles(min) {
     for (let i = 0; i < this.points.length; i++) {
 
-      // adjusts x and y values so that minIndex point is (0,0)
-      let adjustX = this.points[i].x - scaleX;
-      let adjustY = this.points[i].y - scaleY;
+      if (this.points[i][X] !== min[X] || this.points[i][Y] !== min[Y]) {
 
-      // calculates polar angle in radians
-      let angle = Math.atan(adjustY / adjustX);
-      if (!Number.isNaN(angle)) {
-        if (angle < 0) angle += Math.PI;
-        this.points[i].angle = angle;
+        // adjusts x and y values so that minIndex point is (0,0)
+        let adjustX = this.points[i][X] - min[X];
+        let adjustY = this.points[i][Y] - min[Y];
+
+        // calculates polar angle in radians
+        let angle = Math.atan(adjustY / adjustX);
+
+        // adjust angle calculation
+        if (Number.isNaN(angle))
+          angle = Math.PI / 2;
+        else if (angle < 0)
+          angle += Math.PI;
+
+        // set angle
+        this.points[i][ANG] = angle;
       }
     }
+  }
+
+  /**
+   * update animation and wait
+   * @returns {Promise<void>}
+   */
+  async drawAndWait() {
+    this.draw();
+    await new Promise(resolve => setTimeout(resolve, this.delay));
   }
 
   /**
@@ -170,46 +186,51 @@ class ConvexHull {
 
     // draw lines of convex hull
     for (let j = 1; j < this.stack.length; j++) {
-      ctx.beginPath();
       ctx.strokeStyle = "#6483b7";
-      if (this.currentPoint == null && j === this.stack.length - 1) ctx.strokeStyle = "#1058d1";
-      let point1 = this.points[this.stack[j - 1]];
-      let point2 = this.points[this.stack[j]];
-      ctx.moveTo(point1.x + 50, (this.width - 50) - point1.y);
-      ctx.lineTo(point2.x + 50, (this.width - 50) - point2.y);
-      ctx.stroke();
+      if (this.currentPoint == null && j === this.stack.length - 1)
+        ctx.strokeStyle = "#1058d1";
+      let p1 = this.stack[j - 1];
+      let p2 = this.stack[j];
+      this.drawLine(ctx, p1, p2);
     }
 
     // draws line connecting to current point being checked
     if (this.stack.length > 0 && this.currentPoint !== null && !this.finished) {
-      ctx.beginPath();
       ctx.strokeStyle = "#1058d1";
-      let point1 = this.points[this.stack[this.stack.length - 1]];
-      let point2 = this.points[this.currentPoint];
-      ctx.moveTo(point1.x + 50, (this.width - 50) - point1.y);
-      ctx.lineTo(point2.x + 50, (this.width - 50) - point2.y);
-      ctx.stroke();
+      let p1 = this.stack[this.stack.length - 1];
+      let p2 = this.currentPoint;
+      this.drawLine(ctx, p1, p2);
     }
 
     // draw line that closes convex hull
     if (this.finished) {
-      ctx.beginPath();
-      let point1 = this.points[this.stack[0]];
-      let point2 = this.points[this.stack[this.stack.length - 1]];
-      ctx.moveTo(point1.x + 50, (this.width - 50) - point1.y);
-      ctx.lineTo(point2.x + 50, (this.width - 50) - point2.y);
-      ctx.stroke();
+      let p1 = this.stack[0];
+      let p2 = this.stack[this.stack.length - 1];
+      this.drawLine(ctx, p1, p2);
     }
 
     // draw points in set
     for (let i = 0; i < this.points.length; i++) {
-      if (this.points[i].inHull) ctx.fillStyle = "#FFF";
+      if (this.points[i][INC]) ctx.fillStyle = "#FFF";
       else ctx.fillStyle = "#c5d2ea";
       ctx.beginPath();
-      ctx.arc(this.points[i].x + 50, (this.width - 50) - this.points[i].y,
+      ctx.arc(this.points[i][X] + 50, (this.width - 50) - this.points[i][Y],
           5, 0, 2 * Math.PI);
       ctx.fill();
-      ctx.fillText(i.toString(), this.points[i].x + 60, (this.width - 50) - this.points[i].y);
+      ctx.fillText(i.toString(), this.points[i][X] + 60, (this.width - 50) - this.points[i][Y]);
     }
+  }
+
+  /**
+   * draw line from point 1 to point 2
+   * @param ctx - canvas context
+   * @param p1 - point 1
+   * @param p2 - point 2
+   */
+  drawLine(ctx, p1, p2) {
+    ctx.beginPath();
+    ctx.moveTo(p1[X] + 50, (this.width - 50) - p1[Y]);
+    ctx.lineTo(p2[X] + 50, (this.width - 50) - p2[Y]);
+    ctx.stroke();
   }
 }
